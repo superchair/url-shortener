f80bb5f4f9e21edfb184337c09c9d50f72e85ec3
Alex Brown <abrown@mobials.com>
2025-04-17 18:26:21 -0400

    add dependencies (some) and set up config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common'
 import { AppService } from './app.service'
 import { ShortUrlController } from './controllers/app.controller'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { RestAPIUtilities } from '@platform/rest-api-utils'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    RestAPIUtilities.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        appName: configService.get<string>('APP_NAME'),
+        buildNumber: 'UNKNOWN',
+        buildVersion: 'UNKNOWN',
+        appEnv: configService.get<string>('APP_ENV'),
+      }),
+    }),
+  ],
   controllers: [ShortUrlController],
   providers: [AppService],
 })
